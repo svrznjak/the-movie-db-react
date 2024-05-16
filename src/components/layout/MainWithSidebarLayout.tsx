6ec6165c5fb004c9c238ac334e589a78c2f9bbd9
svrznjak <gregor.svrznjak@gmail.com>
2024-05-16 22:46:15 +0200

    Mobile sidebar

--- src/components/layout/MainWithSidebarLayout.tsx
+++ src/components/layout/MainWithSidebarLayout.tsx
@@ -1,16 +1,23 @@
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import styles from './MainWithSidebarLayout.module.css';
+import Button from "../common/Button";
+import { useState } from "react";
 
 
 export default function MainWithSidebarLayout({ title, sidebar, children }: { title: string, sidebar: React.ReactNode, children: React.ReactNode}) {
+  const [isSidebarOpenOnMobile, setIsSidebarOpenOnMobile] = useState(false);
+
   return (
     <>
       <Header />
       <div className={styles.layout}>
-        <h1>{title}</h1>
+        <div className={styles.header}>
+          <h1>{title}</h1>
+          <Button text={isSidebarOpenOnMobile ? 'Less' : 'More'} onClick={()=>{setIsSidebarOpenOnMobile(!isSidebarOpenOnMobile)}} />
+        </div>
         <div className={styles.content}>
-          <div className={styles.sidebar}>{sidebar}</div>
+          <div className={`${styles.sidebar} ${isSidebarOpenOnMobile ? styles.mobileSidebarActive : ''}`}>{sidebar}</div>
           <main>{children}</main>
           </div>
         </div>
