ae76cef9440e54deee6bc9b234415a743d627702
svrznjak <gregor.svrznjak@gmail.com>
2024-05-17 09:33:25 +0200

    Date string error handling

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -3,13 +3,17 @@ import { Movie } from "@/api/thbd";
 import styles from './MovieCard.module.css';
 
 export default function MovieCard({movie}: {movie: Movie}) {
+
+  // Create simple date string. TODO: Use date-fns
+  const dateString = new Date(movie.release_date).toLocaleDateString() !== "Invalid Date" ? new Date(movie.release_date).toLocaleDateString() : '';
+
   return (
     <div className={styles.movieCard}>
       <img src={movie.poster_path !== null ? "https://image.tmdb.org/t/p/w500/"+movie.poster_path : 'https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-grey-c2ebdbb057f2a7614185931650f8cee23fa137b93812ccb132b9df511df1cfac.svg'} alt={movie.title} />
       <div className={styles.content}>
         <div className={styles.rating}>{Math.round(movie.vote_average * 10)}</div>
         <h2>{movie.title}</h2>
-        <span>{new Date(movie.release_date).toLocaleDateString()}</span>
+        <span>{dateString}</span>
       </div>
     </div>
   );
